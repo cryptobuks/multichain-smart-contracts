e7da4622163928c33c9ab3279806fceeecd8c300
bohaos <bohaos@multichain.org>
2022-07-07 15:56:20 +0800

    add price contract and interface

---
+++ contracts/interfaces/PriceOracleInterface.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.15;
+
+interface PriceOracleInterface {
+    event PriceUpdate(uint256 chainID, uint256 price);
+    event DecimalsUpdate(uint256 chainID, uint256 decimal);
+    event InitCurrencyInfo(uint256 chainID, uint256 price, uint256 decimal);
+
+    function setPrice(uint256 chainID, uint112 price) external;
+
+    function getPrice(uint256 chainID) external view returns (uint256 price);
+
+    function setDecimal(uint256 chainID, uint64 decimal) external;
+
+    function getDecimal(uint256 chainID)
+        external
+        view
+        returns (uint256 decimal);
+
+    function initCurrencyInfo(
+        uint256 chainID,
+        uint112 price,
+        uint64 decimal
+    ) external;
+
+    function getCurrencyInfo(uint256 chainID)
+        external
+        view
+        returns (
+            uint256 price,
+            uint256 decimal,
+            uint256 lastUpdateTime
+        );
+}
--- contracts/router/PriceOracleRouter.sol
+++ contracts/router/PriceOracleRouter.sol
@@ -2,39 +2,125 @@
 pragma solidity ^0.8.15;
 
 import "../access/MPCManageable.sol";
+import "../interfaces/PriceOracleInterface.sol";
 
-contract PriceOracleRouter is MPCManageable {
-    mapping(uint256 => uint256) private prices;
-    mapping(uint256 => uint256) private decimals;
+contract PriceOracleRouter is MPCManageable, PriceOracleInterface {
+    // managers of price update
+    mapping(address => bool) private _managers;
+    // currency details
+    mapping(uint256 => CurrencyInfo) private _currencyInfos;
+    // decimal of price
+    uint256 public _decimal;
 
-    event PriceUpdate(uint256 chainID, uint256 price);
-    event DecimalsUpdate(uint256 chainID, uint256 decimal);
+    struct CurrencyInfo {
+        uint112 price;
+        uint64 decimal;
+        uint64 lateUpdateTime;
+    }
 
-    constructor(address _mpc) MPCManageable(_mpc) {}
+    constructor(
+        address mpc_,
+        uint256 decimal_,
+        address[] memory managers_
+    ) MPCManageable(mpc_) {
+        _decimal = decimal_;
+        for (uint256 i = 0; i < managers_.length; i++) {
+            require(!_managers[managers_[i]], "manager exists");
+            require(
+                managers_[i] != address(0),
+                "manager must not equal address zero"
+            );
+            _managers[managers_[i]] = true;
+        }
+    }
 
-    function setPrice(uint256 chainID, uint256 price) external onlyMPC {
-        prices[chainID] = price;
+    modifier onlyManager() {
+        require(_managers[msg.sender], "only manager");
+        _;
+    }
+
+    function setManagersBatch(
+        address[] calldata managers,
+        bool[] calldata availities
+    ) external onlyManager {
+        require(
+            managers.length == availities.length,
+            "managers length must equals availities length"
+        );
+        for (uint256 i = 0; i < managers.length; i++) {
+            setManager(managers[i], availities[i]);
+        }
+    }
+
+    function setManager(address manager, bool availity) public onlyMPC {
+        require(_managers[manager] != availity, "Not change anything");
+        _managers[manager] = availity;
+    }
+
+    function setPricesBatch(
+        uint256[] calldata chainIDs,
+        uint112[] calldata prices
+    ) external onlyManager {
+        require(
+            chainIDs.length == prices.length,
+            "chainIDs length must equals prices length"
+        );
+        for (uint256 i = 0; i < chainIDs.length; i++) {
+            setPrice(chainIDs[i], prices[i]);
+        }
+    }
+
+    function setPrice(uint256 chainID, uint112 price) public onlyManager {
+        CurrencyInfo storage currencyInfo = _currencyInfos[chainID];
+        currencyInfo.price = price;
+        currencyInfo.lateUpdateTime = uint64(block.timestamp);
         emit PriceUpdate(chainID, price);
     }
 
-    function getPrice(uint256 chainID) public view returns (uint256) {
-        return prices[chainID];
+    function getPrice(uint256 chainID) external view returns (uint256 price) {
+        return _currencyInfos[chainID].price;
     }
 
-    function setDecimal(uint256 chainID, uint256 decimal) external onlyMPC {
-        decimals[chainID] = decimal;
+    function setDecimal(uint256 chainID, uint64 decimal) external onlyManager {
+        CurrencyInfo storage currencyInfo = _currencyInfos[chainID];
+        currencyInfo.decimal = decimal;
+        currencyInfo.lateUpdateTime = uint64(block.timestamp);
         emit DecimalsUpdate(chainID, decimal);
     }
 
-    function getDecimal(uint256 chainID) public view returns (uint256) {
-        return decimals[chainID];
+    function getDecimal(uint256 chainID)
+        external
+        view
+        returns (uint256 decimal)
+    {
+        return _currencyInfos[chainID].decimal;
+    }
+
+    function initCurrencyInfo(
+        uint256 chainID,
+        uint112 price,
+        uint64 decimal
+    ) external onlyManager {
+        CurrencyInfo storage currencyInfo = _currencyInfos[chainID];
+        currencyInfo.price = price;
+        currencyInfo.decimal = decimal;
+        currencyInfo.lateUpdateTime = uint64(block.timestamp);
+        emit InitCurrencyInfo(chainID, price, decimal);
     }
 
     function getCurrencyInfo(uint256 chainID)
-        public
+        external
         view
-        returns (uint256, uint256)
+        returns (
+            uint256 price,
+            uint256 decimal,
+            uint256 lastUpdateTime
+        )
     {
-        return (prices[chainID], decimals[chainID]);
+        return (
+            _currencyInfos[chainID].price,
+            _currencyInfos[chainID].decimal,
+            _currencyInfos[chainID].lateUpdateTime
+        );
     }
 }
